372dc96756183210b7d5ba3c050181b37d04fc44
asaduzzaman shahin <ashahin1995@gmail.com>
2023-11-21 02:22:58 +0600

    Initial commit

---
+++ lib/screens/login.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+class login extends StatefulWidget {
+  const login({super.key});
+
+  @override
+  State<login> createState() => _loginState();
+}
+
+class _loginState extends State<login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title:Text('Login'),),
+      body: SingleChildScrollView(
+        child: Container(),
+      ),
+
+
+    );
+  }
+}
